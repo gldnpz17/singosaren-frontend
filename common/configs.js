da0ee29af64217de85af5827885e60913af1afba
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2022-07-19 17:23:00 +0700

    Change the default ISR revalidation timeout.

--- common/configs.js
+++ common/configs.js
@@ -1,5 +1,5 @@
 const configs = {
-  isrDurationSeconds: process.env.ISR_DURATION_SECONDS ? Number.parseInt(process.env.ISR_DURATION_SECONDS) : 60 * 60 * 12
+  isrDurationSeconds: process.env.ISR_DURATION_SECONDS ? Number.parseInt(process.env.ISR_DURATION_SECONDS) : 10
 }
 
 export default configs
