d93af4b74e3d569dfb213532f96e48f97be26802
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2022-07-23 20:50:38 +0700

    Changed the HorizontalContentCard layout for mobile.

--- components/HorizontalContentCard.js
+++ components/HorizontalContentCard.js
@@ -9,15 +9,16 @@ export default function HorizontalContentCard({ content: { id, shortDescription,
   return (
     <div
       onClick={handleClick}
-      className='shadow-md rounded hover:shadow-2xl duration-300 h-full bg-indigo-50 overflow-hidden cursor-pointer flex'
+      className='md:shadow-md rounded hover:bg-indigo-50 md:hover:shadow-2xl duration-300 h-full bg-white md:bg-indigo-50 overflow-hidden cursor-pointer flex'
     >
-      <img src={coverImageUrl} className='h-48 w-64 object-cover hidden md:block' />
+      <img src={coverImageUrl} className='h-32 w-32 md:shadow-none md:h-48 md:w-64 object-cover' />
       <div className='px-4 pt-2 pb-4 flex flex-col flex-grow'>
         <div className='mt-1 flex space-x-2'>
           {tags.map(tag => <ContentCardTag key={tag.id} tag={tag} />)}
         </div>
         <div className='font-bold text-xl font-sans-serif'>{title}</div>
-        <div className='flex-grow overflow-hidden'>
+        <div className='flex-grow md-block md:hidden' />
+        <div className='flex-grow overflow-hidden hidden md:block'>
           <div className='line-clamp-3'>
             {shortDescription}
           </div>
